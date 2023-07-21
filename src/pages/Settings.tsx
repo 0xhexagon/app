83d6c02612bf379b705242754f9eac9c7338d2c0
Manuel Rivera <menu.rivera@gmail.com>
2023-07-21 14:49:36 -0600

    [MOD] -> replaced settigns back button with IonBackButton

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,21 +1,15 @@
-import { IonButton, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react'
-import { arrowBackOutline } from 'ionicons/icons'
+import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react'
 import React from 'react'
 
 const Settings: React.FC = () => {
-	const router = useIonRouter()
-
-	const goBack = () => {
-		router.goBack() 
-	}
 	return (
 		<IonPage>
 			<IonHeader>
 				<IonToolbar>
 					<IonTitle>Settings</IonTitle>
-					<IonButton onClick={goBack} slot='start' fill='clear' size='small'>
-						<IonIcon slot='icon-only' icon={arrowBackOutline} />
-					</IonButton>
+					<IonButtons slot='start'>
+						<IonBackButton defaultHref='/app' />
+					</IonButtons>
 				</IonToolbar>
 			</IonHeader>
 			<IonContent className='ion-padding'>
