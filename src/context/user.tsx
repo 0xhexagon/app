d3ab41439f9428876dfd4079b69e9c0560086f62
Manuel Rivera <menu.rivera@gmail.com>
2023-07-23 22:20:07 -0600

    [ADD] -> UserContext created with custom useUser hook

---
+++ src/context/user.tsx
@@ -0,0 +1,53 @@
+import { createContext, useContext, useState } from "react"
+
+export class User {
+	id: string
+	publicKey: string
+	name: string
+	username: string
+	bio?: string
+	profileNft?: string
+	poaps?: string[]
+
+	constructor() {
+		this.id = ''
+		this.publicKey = ''
+		this.name = ''
+		this.username = ''
+	}
+}
+
+//Context with default value
+interface UserContextType {
+	user: User,
+	setUser: (u: User) => void
+}
+const UserContext = createContext<UserContextType>({
+	user: new User(),
+	setUser: () => {}
+})
+
+// Hook  
+export const useUser = () => {
+	const {user, setUser} = useContext(UserContext)
+	return {
+		user: user.id ? user : null,
+		setUser
+	}	
+}
+
+// Provider
+interface Props {
+	children: React.ReactNode
+}
+const UserProvider: React.FC<Props> = ({ children }) => {
+	const [user, setUser] = useState<User>(new User())
+	
+	return (
+		<UserContext.Provider value={{user, setUser}}>
+			{children}
+		</UserContext.Provider>
+	)
+}
+
+export default UserProvider
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import PolybaseProviders from './context/polybase';
+import UserProvider from './context/user';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
 	<React.StrictMode>
 		<PolybaseProviders>
-			<App />
+			<UserProvider>
+				<App />
+			</UserProvider>
 		</PolybaseProviders>
 	</React.StrictMode>
 );
